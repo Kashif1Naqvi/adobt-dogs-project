3703e603d3f66d387747fdf0b2a8e32e7c384c42
Kashif1Naqvi <mrkashif029@gmail.com>
2020-12-30 01:06:10 +0500

    code modified by functions

---
+++ src/App.js
@@ -0,0 +1,23 @@
+const Pet = ({name,animal,breed}) => {
+    return React.createElement("div",{},[
+        React.createElement("h1",{},name),
+        React.createElement("h2",{},animal),
+        React.createElement("h2",{},breed)
+    ])
+}
+
+
+const App = () => {
+    return React.createElement(
+        'div',
+        {}, 
+        React.createElement('h1',{},"Adopt me!"),
+        React.createElement(Pet,{name:'pepper',animal:'Bird',breed:'Cockitiel'}),
+        React.createElement(Pet,{name:'Orio',animal:'Dog',breed:'Havenes'}),
+        React.createElement(Pet,{name:'Doink',animal:'Dog',breed:'Mixed'})
+    )
+}
+ReactDOM.render(
+    React.createElement(App),
+    document.getElementById('root')
+)
