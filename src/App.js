5a2f68ec9af825be0e6c8c95cd615ad3bc79ba03
Kashif1Naqvi <mrkashif029@gmail.com>
2021-01-01 08:38:49 +0500

    setting bebel react eslint with convert react-element to jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import {render} from 'react-dom'
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
-
+import  Pet  from './Pet';
 const App = () => {
-    return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "pepper",
-      animal: "Bird",
-      breed: "Cockitiel",
-    }),
-    React.createElement(Pet, { name: "Orio", animal: "Dog", breed: "Havenes" }),
-    React.createElement(Pet, { name: "Doink", animal: "Dog", breed: "Mixed" })
-  );
+  //   return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", {}, "Adopt me!"),
+  //   React.createElement(Pet, {
+  //     name: "pepper",
+  //     animal: "Bird",
+  //     breed: "Cockitiel",
+  //   }),
+  //   React.createElement(Pet, { name: "Orio", animal: "Dog", breed: "Havenes" }),
+  //   React.createElement(Pet, { name: "Doink", animal: "Dog", breed: "Mixed" })
+  // );
+  return(
+    <div>
+       <h1 id="something-important" >Adobt me!</h1>
+       <Pet name="Pepper" animal="Bird" breed="Cockitiel" />
+       <Pet name="Oria" animal="Dog" breed="Havens" />
+       <Pet name="Doink" animal="Dog" breed="Mixed" />
+
+    </div>
+  )
 };
 render(React.createElement(App), document.getElementById("root"));
